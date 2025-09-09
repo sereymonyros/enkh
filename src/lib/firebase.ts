e5af0635d430a09c00502b3e6e1849736425c009
Sereymony Ros <sereymonyros@gmail.com>
2025-09-09 22:24:23 +0000

    yes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,11 +5,11 @@ import {getFirestore} from 'firebase/firestore';
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "enkh-ovr0a.firebaseapp.com",
-  projectId: "enkh-ovr0a",
-  storageBucket: "enkh-ovr0a.firebasestorage.app",
-  messagingSenderId: "797985931687",
-  appId: "1:797985931687:web:938b1dce3a931f67447682"
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 
