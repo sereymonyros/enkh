bc806fb4f017990c3ef13cdfdd4ebf0e49631f8b
Sereymony Ros <sereymonyros@gmail.com>
2025-09-10 17:33:51 +0000

    make border color 2px, and use this class for border color .ring-offset-

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -243,7 +243,7 @@ const Sidebar = React.forwardRef<
             // Adjust the padding for floating and inset variants.
             variant === "floating" || variant === "inset"
               ? "p-2 group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)_+_theme(spacing.4)_+2px)]"
-              : "group-data-[collapsible=icon]:w-[--sidebar-width-icon] group-data-[side=left]:border-r group-data-[side=right]:border-l",
+              : "group-data-[collapsible=icon]:w-[--sidebar-width-icon] group-data-[side=left]:border-r-2 group-data-[side=right]:border-l-2",
             className
           )}
           {...props}
