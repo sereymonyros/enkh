ff76507791a91bca0497069754ae521cab4b1199
Sereymony Ros <sereymonyros@gmail.com>
2025-09-12 16:50:50 +0000

    swap icon between dark and light (_for element <Button>_)

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -23,8 +23,8 @@ export function ThemeToggle() {
       size="icon"
       onClick={handleThemeChange}
     >
-      <Sun className="h-[1.5rem] w-[1.3rem] text-blue-400 opacity-100 dark:hidden" />
-      <Moon className="hidden h-5 w-5 text-blue-400 opacity-100 dark:block" />
+      <Moon className="h-[1.5rem] w-[1.3rem] text-blue-400 opacity-100 dark:hidden" />
+      <Sun className="hidden h-5 w-5 text-blue-400 opacity-100 dark:block" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   )
