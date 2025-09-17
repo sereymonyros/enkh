91aaf5850079ca042e470cead1328a0d80f26213
Sereymony Ros <sereymonyros@gmail.com>
2025-09-17 05:23:57 +0000

    path is blue, the same blue from GoogleIcon (_for element <FacebookIcon>

--- src/components/icons/facebook-icon.tsx
+++ src/components/icons/facebook-icon.tsx
@@ -7,7 +7,7 @@ export const FacebookIcon = (props: SVGProps<SVGSVGElement>) => (
     width="1em"
     height="1em"
     viewBox="0 0 24 24"
-    fill="currentColor"
+    fill="#1976D2"
     {...props}
   >
     <path d="M9.19795 21.5H13.198V12.5H16.198L17.198 9.5H13.198V7.5C13.198 6.94772 13.6457 6.5 14.198 6.5H17.198V2.5H14.198C11.4365 2.5 9.19795 4.73858 9.19795 7.5V9.5H6.19795V12.5H9.19795V21.5Z" />
